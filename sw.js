0069b786b5ddc432ec93bbc5bcc0296eae45712e
Build New Habits <buildnewhabits@outlook.com>
2026-04-26 15:29:07 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
  * Old caches are deleted on activate so users always get the latest shell.
  */
 
-const CACHE_NAME = "alongside-v29";
+const CACHE_NAME = "alongside-v32";
 
 const SHELL_URLS = [
   "/alongside-app/",
