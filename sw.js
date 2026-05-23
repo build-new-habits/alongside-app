0806806fe7e3395ccde231ed1033c4f9552fb7a1
Build New Habits <buildnewhabits@outlook.com>
2026-05-23 10:09:50 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * sw.js - Alongside Service Worker
  *
  * 23 May 2026 v3
- * Cache version: alongside-v91
+ * Cache version: alongside-v92
  *
  * v86 changes (23 May 2026)
  * - Cosmetic Fixes
@@ -143,7 +143,7 @@
  * sw.js must always be the LAST file deployed in any batch.
  */
 
-const CACHE_NAME = "alongside-v91";
+const CACHE_NAME = "alongside-v92";
 
 const SHELL_URLS = [
 
